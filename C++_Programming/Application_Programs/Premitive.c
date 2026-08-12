3cf83029336a25a360caae7da604da354c646d78
AadityaMundhe02 <mundheaaditya01@gmail.com>
2026-08-12 16:05:02 +0530

    Add files via upload

---
+++ C++_Programming/Application_Programs/Premitive.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main()
+{   
+    char ch = 'A';
+    int no = 11;
+    float x = 3.14f;
+    double d = 9.123456;
+
+    printf("R Values of all Variables\n");
+
+    printf("%c\n",ch);
+    printf("%d\n",no);
+    printf("%f\n",x);
+    printf("%lf\n",d);
+
+    return 0;
+}
