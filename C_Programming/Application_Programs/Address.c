61c0ad0c9ee4b0c33f1e62e143b2fb38991cad3c
AadityaMundhe02 <mundheaaditya01@gmail.com>
2026-08-13 23:32:58 +0530

    Add files via upload

---
+++ C_Programming/Application_Programs/Address.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main()
+{
+    char ch = 'A';
+    int no = 21;
+    float x = 3.14f;
+    double d = 99.987654;
+
+    printf("L Value of Variables is : \n");
+
+    printf("%u\n",&ch);
+    printf("%u\n",&no);
+    printf("%u\n",&x);
+    printf("%u\n",&d);
+
+    return 0;
+}
